0f3ce8ab3edc1b4ede5822c64134287db031d535
在路上 <3414048190@qq.com>
2025-01-12 15:32:28 +0800

    add data.py

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    'model': 'bert',
+    'pretrained_model': 'bert-base-chinese',
+    'vocab_size': 10000,
+    'hidden_size': 768,
+    'num_layers': 2,
+    'input_size': 768,
+    'output_size': 10,
+    'dropout_prob': 0.5,
+    'num_tags': 10,
+    'CRF': True
+}
---
+++ data.py
@@ -0,0 +1,113 @@
+import os
+from config import config
+import torch
+import pickle
+## tags, BIO
+tag2label = {"O": 0,
+             "B-PER": 1, "I-PER": 2,
+             "B-LOC": 3, "I-LOC": 4,
+             "B-ORG": 5, "I-ORG": 6
+             }
+config["num_tags"] = len(tag2label)
+"""
+先把数据处理完成吧
+然后再写一个loader类
+"""
+def read_corpus(corpus_path):
+    data = []
+    sens ,tags = [],[]
+    with open(corpus_path, encoding="utf-8") as f:
+        for line in f:
+            if line != "\n":
+                token, tag = line.strip().split()
+                sens.append(token)
+                tags.append(tag)
+            else:
+                data.append([sens,tag])
+                sens,tags = [],[]
+    return data
+def vocab_build(vocab_path, corpus_path, min_count):
+    """
+
+    :param vocab_path:
+    :param corpus_path:
+    :param min_count:
+    :return:
+    """
+    data = read_corpus(corpus_path)
+    word2id = {}
+    for sent_, tag_ in data:
+        for word in sent_:
+            if word.isdigit():
+                word = '<NUM>'
+            elif ('\u0041' <= word <='\u005a') or ('\u0061' <= word <='\u007a'):
+                word = '<ENG>'
+            if word not in word2id:
+                word2id[word] = [len(word2id)+1, 1]
+            else:
+                word2id[word][1] += 1
+    low_freq_words = []
+    for word, [word_id, word_freq] in word2id.items():
+        if word_freq < min_count and word != '<NUM>' and word != '<ENG>':
+            low_freq_words.append(word)
+    for word in low_freq_words:
+        del word2id[word]
+
+    new_id = 1
+    for word in word2id.keys():
+        word2id[word] = new_id
+        new_id += 1
+    word2id['<UNK>'] = new_id
+    word2id['<PAD>'] = 0
+
+    print(len(word2id))
+    with open(vocab_path, 'wb') as fw:
+        pickle.dump(word2id, fw)
+
+
+def sentence2id(sent, word2id):
+    """
+
+    :param sent:
+    :param word2id:
+    :return:
+    """
+    sentence_id = []
+    for word in sent:
+        if word.isdigit():
+            word = '<NUM>'
+        elif ('\u0041' <= word <= '\u005a') or ('\u0061' <= word <= '\u007a'):
+            word = '<ENG>'
+        if word not in word2id:
+            word = '<UNK>'
+        sentence_id.append(word2id[word])
+    return sentence_id
+
+
+def read_vocab(vocab_path):
+    """
+
+    :param vocab_path:
+    :return:
+    """
+    vocab_path = os.path.join(vocab_path)
+    with open(vocab_path, 'rb') as fr:
+        word2id = pickle.load(fr)
+    print('vocab_size:', len(word2id))
+    return word2id
+
+def pad_sequences(sequences, pad_mark=0):
+    """
+
+    :param sequences:
+    :param pad_mark:
+    :return:
+    """
+    max_len = max(map(lambda x : len(x), sequences))
+    seq_list, seq_len_list = [], []
+    for seq in sequences:
+        seq = list(seq)
+        seq_ = seq[:max_len] + [pad_mark] * max(max_len - len(seq), 0)
+        seq_list.append(seq_)
+        seq_len_list.append(min(len(seq), max_len))
+    return seq_list, seq_len_list
